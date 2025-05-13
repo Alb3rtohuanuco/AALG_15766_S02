48abaee8c69b52a56612c3f10de58d905b3248b6
Alb3rtohuanuco <208847748+Alb3rtohuanuco@users.noreply.github.com>
2025-05-13 16:12:14 -0500

    funciones commit

---
+++ funciones.py
@@ -0,0 +1,20 @@
+a=len("Jherson")
+print(a)
+
+b=len(["Carlos",3,4,5,True])
+print(b)
+
+c=chr(65)
+print(c)
+
+edad = 20
+if edad >= 15 and edad <= 30:
+    print("ok")
+
+
+d=abs(-35)
+print(d)    
+
+e=id(a)
+print(e)
+
