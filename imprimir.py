a712fada7ac8798b9e8f6e6153fa515bcfd77d96
Alb3rtohuanuco <208847748+Alb3rtohuanuco@users.noreply.github.com>
2025-05-13 12:48:55 -0500

    archivos imprimir e ingreso

---
+++ imprimir.py
@@ -0,0 +1,16 @@
+a = "Carlos"
+b = 29
+
+print('Hola ', a, 'tienes', b)
+
+c= "Hola "+ str(a) + "tienes " + str(b)
+print(c)
+
+#mejor recomedada por ser mas concisa y corta
+d = f"hola {a} tienes {b}"
+print(d)
+
+print("Jherson", "Jose", "Juan")
+print("Jherson", "Jose", "Juan", sep="*", end="%\n")
+print("Jherson", "jose","juan")
+
---
+++ program.py
@@ -0,0 +1,26 @@
+#operadores y condicionales
+a= 2**3+1
+print(a)
+
+if a==8:
+    print("True")
+else:
+    print("False")
+
+print("Fin")
+
+#concatenación de textos
+b= 'hola' + ' amigo'
+print(b)
+
+c= 'jherson ' * 5
+print(c)
+
+
+#comentario en una linea
+
+'''
+comentario
+en 
+varias lineas
+'''
